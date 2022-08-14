ae09540ad8448801c3c6ddda78626dbedc7ffd8c
Victor Leone de Oliveira <vileoleone@gmail.com>
2022-08-14 11:37:23 -0300

    Versão com correções e ajustes fino no código

--- Script.js
+++ Script.js
@@ -12,32 +12,32 @@ function typeOfCuisine (clickedBox) {
 //Function to turn style on clicked box
 function styleOn(boxElement, IconElement) {
    boxElement.classList.toggle("selectedStyle");
-   IconElement.classList.toggle("hideIcon")
-   IconElement.classList.toggle("online")
+   IconElement.classList.toggle("hideIcon");
+   IconElement.classList.toggle("online");
 }
 //Function to turn style off clicked box
 function styleOff(boxElement, IconElement) {
    boxElement.classList.toggle("selectedStyle");
-   IconElement.classList.toggle("online")
-   IconElement.classList.toggle("hideIcon")
+   IconElement.classList.toggle("online");
+   IconElement.classList.toggle("hideIcon");
 }
 let footerBox = document.querySelector(".order-box")
 function styleFooter() {
    footerBox.style.backgroundColor = "#32B72F";
-   footerBox.querySelector("p").innerHTML = "Fechar o pedido"
-   footerBox.querySelector("p").style.fontWeight = "700"
+   footerBox.querySelector("p").innerHTML = "Fechar o pedido";
+   footerBox.querySelector("p").style.fontWeight = "700";
 }
 function initialStyleFooter() {
    footerBox.style.backgroundColor = "#CBCBCB";
-   footerBox.querySelector("p").innerHTML = "Selecione os 3 itens <br> para fechar o pedido"
-   footerBox.querySelector("p").style.fontWeight = "400"
+   footerBox.querySelector("p").innerHTML = "Selecione os 3 itens <br> para fechar o pedido";
+   footerBox.querySelector("p").style.fontWeight = "400";
 }
 
 function verifyNumberOfChoices () {
    if(document.querySelectorAll(".selectedStyle").length == 3) {
       styleFooter();
    } else{ 
-      initialStyleFooter()
+      initialStyleFooter();
    }
 }
 
@@ -52,7 +52,7 @@ function optionClick(boxChosen) {
    if (boxChosen.classList.contains("selectedStyle")) {
       let IconToHide = boxChosen.querySelector(".online");
       styleOff(boxChosen, IconToHide);
-      verifyNumberOfChoices() 
+      verifyNumberOfChoices();
       return;
    }
    if (verifyVariable !=  null) {
@@ -60,12 +60,12 @@ function optionClick(boxChosen) {
          //Desestilizar o previamente selecionado
          styleOff(verifyVariable, verifyIcon);
          // Estilizar o selecionado
-         styleOn(boxChosen, IconToDisplay)
+         styleOn(boxChosen, IconToDisplay);
         // Caso o selecionado esteja em grupo diferente do previament selecionado 
-      } else {styleOn(boxChosen, IconToDisplay)}
+      } else {styleOn(boxChosen, IconToDisplay)};
    } 
    //Caso seja o primeiro clique do usuário
-   else {styleOn(boxChosen, IconToDisplay)}
+   else {styleOn(boxChosen, IconToDisplay)};
    //Verificando se o pedido foi feito de maneira completa
    verifyNumberOfChoices() 
 }
