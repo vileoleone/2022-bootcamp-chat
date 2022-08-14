196d6ce475c57b96e06a83ce6db22052ae2b4a96
Victor Leone de Oliveira <vileoleone@gmail.com>
2022-08-14 16:51:23 -0300

    Versão Final

--- Script.js
+++ Script.js
@@ -4,8 +4,8 @@ function typeOfCuisine (clickedBox) {
    let cuisineSelector;
    let secondClass = clickedBox.classList[1];
    if (secondClass == "food") {cuisineType = ".foodCuisine"}
-   else if (secondClass =="drink") {cuisineType = ".drinkCuisine"} 
-   else { cuisineType = ".sweetCuisine"}
+   else if (secondClass =="drink") {cuisineType = ".drinkCuisine"}; 
+   else { cuisineType = ".sweetCuisine"};
    cuisineSelector = document.querySelector(cuisineType);
    return cuisineSelector;
 }
@@ -21,7 +21,8 @@ function styleOff(boxElement, IconElement) {
    IconElement.classList.toggle("online");
    IconElement.classList.toggle("hideIcon");
 }
-let footerBox = document.querySelector(".order-box")
+let footerBox = document.querySelector(".order-box");
+//
 function styleFooter() {
    footerBox.style.backgroundColor = "#32B72F";
    footerBox.querySelector("p").innerHTML = "Fechar o pedido";
@@ -32,17 +33,16 @@ function initialStyleFooter() {
    footerBox.style.backgroundColor = "#CBCBCB";
    footerBox.querySelector("p").innerHTML = "Selecione os 3 itens <br> para fechar o pedido";
    footerBox.querySelector("p").style.fontWeight = "400";
-   footerBox.classList.remove,("ready-to-close")
+   footerBox.classList.remove,("ready-to-close");
 }
-
+//
 function verifyNumberOfChoices () {
-   if(document.querySelectorAll(".selectedStyle").length == 3) {
+   if (document.querySelectorAll(".selectedStyle").length == 3) {
       styleFooter();
-   } else{ 
+   }else { 
       initialStyleFooter();
    }
 }
-
 //Function to apply tests and give client visual feedback of his/her choices 
 function optionClick(boxChosen) {
    let cuisineSet = typeOfCuisine(boxChosen);
@@ -63,12 +63,12 @@ function optionClick(boxChosen) {
          // Estilizar o selecionado
          styleOn(boxChosen, IconToDisplay);
         // Caso o selecionado esteja em grupo diferente do previament selecionado 
-      } else {styleOn(boxChosen, IconToDisplay)};
+      }else {styleOn(boxChosen, IconToDisplay)};
    } 
    //Caso seja o primeiro clique do usuário
-   else {styleOn(boxChosen, IconToDisplay)};
+      else {styleOn(boxChosen, IconToDisplay)};
    //Verificando se o pedido foi feito de maneira completa
-   verifyNumberOfChoices() 
+   verifyNumberOfChoices();
    linkToContact();
 }
 
@@ -83,21 +83,21 @@ function linkToContact() {
 }
 
 function constructString () {
-   let cuisinesToClose = document.querySelectorAll(".selectedStyle")
-   let foodToClose =cuisinesToClose[0].querySelector("p2").innerHTML
-   let drinkToClose =cuisinesToClose[1].querySelector("p2").innerHTML
-   let sweetToClose =cuisinesToClose[2].querySelector("p2").innerHTML
+   let cuisinesToClose = document.querySelectorAll(".selectedStyle");
+   let foodToClose =cuisinesToClose[0].querySelector("p2").innerHTML;
+   let drinkToClose =cuisinesToClose[1].querySelector("p2").innerHTML;
+   let sweetToClose =cuisinesToClose[2].querySelector("p2").innerHTML;
    //
-   let foodPrice = Number(cuisinesToClose[0].querySelector("p4").innerHTML.replace(/[a-z]|[$]/gi,"").replace(/[,]/,"."))
-   let drinkPrice = Number(cuisinesToClose[1].querySelector("p4").innerHTML.replace(/[a-z]|[$]/gi,"").replace(/[,]/,"."))
-   let sweetPrice = Number(cuisinesToClose[2].querySelector("p4").innerHTML.replace(/[a-z]|[$]/gi,"").replace(/[,]/,"."))
+   let foodPrice = Number(cuisinesToClose[0].querySelector("p4").innerHTML.replace(/[a-z]|[$]/gi,"").replace(/[,]/,"."));
+   let drinkPrice = Number(cuisinesToClose[1].querySelector("p4").innerHTML.replace(/[a-z]|[$]/gi,"").replace(/[,]/,"."));
+   let sweetPrice = Number(cuisinesToClose[2].querySelector("p4").innerHTML.replace(/[a-z]|[$]/gi,"").replace(/[,]/,"."));
    //
    let totalPrice = foodPrice + drinkPrice + sweetPrice; 
    let partialString = `Olá, gostaria de fazer o pedido:
-   - Prato: ${foodToClose}
-   - Bebida: ${drinkToClose}
-   - Sobremesa: ${sweetToClose}
-   Total: R$ ${totalPrice.toFixed(2)}`
+   - Prato: ${foodToClose};
+   - Bebida: ${drinkToClose};
+   - Sobremesa: ${sweetToClose};
+   Total: R$ ${totalPrice.toFixed(2)}`;
    //
    let totalString = encodeURIComponent(partialString);
    return totalString;
