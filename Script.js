c5653a9df16ca264c13594b32519c978f7e684e5
Victor Leone de Oliveira <vileoleone@gmail.com>
2022-08-16 22:24:31 -0300

    Adição da função para mostrar o número de cartas escolhidas e shuffle das mesmas no display do usuário

--- Script.js
+++ Script.js
@@ -1,21 +1,43 @@
 let  cardNumber;
-let gifArray = []
 let mainDiv = document.querySelector(".main-box");
+let cardsList =document.querySelectorAll(".card-box")
+//
+function comparador() { 
+	return Math.random() - 0.5; 
+}
 //Função inicial para montar layout do jogo de acordo com a escolha do jogador
 function initialNumberInput () {
     cardNumber = Number(prompt("Escolha com quantas cartas você quer jogar, por favor digite um número par entre 4 e 14"));
-    if (cardNumber < 4 || cardNumber > 7) {
-            alert("Por favor digite um número de cartas entre 4 e 7")
+    if (cardNumber < 4 || cardNumber > 14) {
+            alert("Por favor digite um número de cartas entre 4 e 7");
             initialNumberInput();
         }
     if (cardNumber % 2 !== 0 ) {
-        alert("Por favor digite um número par entre 4 e 7")
+        alert("Por favor digite um número par entre 4 e 7");
         initialNumberInput();
     }
-    //for (let index = 0; index <= 2*cardNumber; index++) {
-          // adicionar child e colocar  classe "card-box"
-          //adicionar link, img com o gifs
-    //}
+    showAndShuffle(cardNumber);
+    mainDiv.style.width = `${80*cardNumber + 70}px`
 }
 
-initialNumberInput();
+function showAndShuffle (number) {
+    let arrayToShuffle = []
+    for (let index = 0; index <cardNumber; index++) {
+        arrayToShuffle.push(cardsList[index]);
+    }
+    arrayToShuffle.sort(comparador)
+    for (let index = 0; index < arrayToShuffle.length; index++) {
+        arrayToShuffle[index].classList.remove("hidden")
+    }
+}
+initialNumberInput ()
+
+function flipCardAndCompare(element) {
+    
+}
+
+
+
+
+
+
